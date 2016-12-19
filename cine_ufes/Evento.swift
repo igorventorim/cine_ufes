be77b03e033ac3765b4838617309d6e102aa4961
Vinicius Risso <viniciusrisso@hotmail.com>
2016-12-19 10:42:19 -0200

    Implementei as classes Titulo e Evento

---
+++ cine_ufes/Evento.swift
@@ -0,0 +1,21 @@
+//
+//  Evento.swift
+//  cine_ufes
+//
+//  Created by Student on 12/19/16.
+//  Copyright © 2016 ufes.br. All rights reserved.
+//
+
+import Foundation
+
+class Evento {
+    
+    var dia:String?
+    var hora:String?
+    
+    init (dia:String, hora:String){
+    
+        self.dia = dia
+        self.hora = hora
+    }
+}
