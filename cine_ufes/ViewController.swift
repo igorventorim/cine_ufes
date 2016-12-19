6b68e842e651cfe53a363bcfac8a7292b28fab6b
cassianomaia <cassmala@gmail.com>
2016-12-19 08:53:53 -0200

    Initial Commit

---
+++ cine_ufes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cine_ufes
+//
+//  Created by Student on 12/19/16.
+//  Copyright © 2016 ufes.br. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
